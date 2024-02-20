534a4f509d24a99659e56651cf6e440276e7a18e
loren <111726817+lorennnzzoo@users.noreply.github.com>
2024-02-20 15:56:35 +0530

    creating login and register method in account controller

--- manasamudram-api/manasamudram-api/Controllers/AccountController.cs
+++ manasamudram-api/manasamudram-api/Controllers/AccountController.cs
@@ -4,10 +4,64 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Models;
 
 namespace manasamudram_api.Controllers
 {
     public class AccountController : ApiController
     {
+
+        private ManasamudramEntities dbContext = new ManasamudramEntities(); 
+
+        [HttpPost]
+        [Route("api/register")]
+        public IHttpActionResult Register(App_Users newUser)
+        {
+            try
+            {
+                if (newUser == null)
+                    return BadRequest("Invalid data");
+
+                
+                if (dbContext.App_Users.Any(u => u.UserName == newUser.UserName))
+                    return Conflict(); 
+
+                
+                dbContext.App_Users.Add(newUser);
+                dbContext.SaveChanges();
+
+                return Ok("Registration successful");
+            }
+            catch (Exception ex)
+            {
+                
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/login")]
+        public IHttpActionResult Login(App_Users loginUser)
+        {
+            try
+            {
+                if (loginUser == null)
+                    return BadRequest("Invalid data");
+
+                
+                var user = dbContext.App_Users.FirstOrDefault(u => u.UserName == loginUser.UserName && u.Password == loginUser.Password);
+
+               
+                if (user != null)
+                    return Ok("Login successful");
+                else
+                    return Unauthorized(); 
+            }
+            catch (Exception ex)
+            {
+               
+                return InternalServerError(ex);
+            }
+        }
     }
 }
