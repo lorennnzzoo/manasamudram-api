91c32104acec7797be2f85ed26b7cdbdddb409c7
loren <111726817+lorennnzzoo@users.noreply.github.com>
2024-02-21 10:46:26 +0530

    added trip controller and class in repository ado
    added trip controller and tripoperation method in controllers and repositoryado to work with trip operations

---
+++ manasamudram-api/RepositoryADO/TripOperations.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryADO
+{
+    public class TripOperations
+    {
+        private ManasamudramEntities context = new ManasamudramEntities();
+        public string connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["ManasamudramString"].ToString();
+
+        public string GetTripCount(string UserName)
+        {
+            DateTime currentDate = DateTime.Today;
+
+            string queryCount = "SELECT TOP 1 NoOfTrips FROM Trips WHERE DriveName = @DriverName AND DateandTime = @CurrentDate ORDER BY NoOfTrips DESC;";
+
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand commandCount = new SqlCommand(queryCount, connection))
+                {
+                    commandCount.Parameters.AddWithValue("@DriverName", UserName);
+                    commandCount.Parameters.AddWithValue("@CurrentDate", currentDate.ToString("yyyy-MM-dd"));
+
+                    connection.Open();
+                    var result = commandCount.ExecuteScalar();
+                    
+                    if (result != null)
+                    {
+                        return Convert.ToString( result);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ manasamudram-api/manasamudram-api/Controllers/TripController.cs
@@ -0,0 +1,38 @@
+﻿using RepositoryADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace manasamudram_api.Controllers
+{
+    [RoutePrefix("api/Trip")]
+    public class TripController : ApiController
+    {
+        TripOperations Top = new TripOperations();
+
+        [HttpGet]
+        [Route("GetTripCount")]
+        public IHttpActionResult GetTrip(string userName)
+        {
+            try
+            {
+                string trips = Top.GetTripCount(userName);
+                if(trips != null)
+                {
+                    return Ok(trips);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {       
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
